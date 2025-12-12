7cc210fe306b7f0bc002d65edb90797ea539e593
Vlad <76049840+vplot96@users.noreply.github.com>
2025-12-13 02:49:35 +0300

    Delete cmd_help.py

--- cmd_help.py
+++
@@ -1,5 +0,0 @@
-from telegram import Update
-from telegram.ext import ContextTypes
-
-async def help_cmd(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Доступные команды:\n/balance – узнать свой баланс\n/price <название товара> – узнать текущий курс товара\n/pay <имя пользователя> <сумма> – сделать перевод\n/ops <число> – посмотреть последние операции")
